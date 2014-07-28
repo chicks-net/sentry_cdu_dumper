f1366a638673eef16b53c1507d512ee5abacea02
Christopher Hicks <chicks.net@gmail.com>
2014-07-28 14:10:23 -0700

    python: less (verbosity) is more

--- sentry-cdu-dumper.py
+++ sentry-cdu-dumper.py
@@ -31,7 +31,7 @@ pdu_name = args[0]
 # dump PDU stats
 tn = telnetlib.Telnet(pdu_name, 23, 10)
 pdu_pass = os.getenv('PDU_PASS', 'admn') 
-print "# connected to " + pdu_name + "... logging in..."
+print "# connected to " + pdu_name + "..."
 
 tn.read_until("Username: ")
 tn.write("admn\n")
